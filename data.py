e0924477adef6174bea06dfd811cbf35d77b94c5
Daniel Kramer <danielkramer@MacBook-Air-3M.hsd1.il.comcast.net>
2024-07-27 23:30:33 -0500

    web scraping data

--- data.py
+++ data.py
@@ -1,1 +1,13 @@
-#hey
+import pandas as pd
+import datetime as dt
+import yfinance as yf
+
+def getStockData(ticker):
+    # Set time frame for training data
+    end = dt.datetime.now()
+    start = end - dt.timedelta(days=365)
+
+    # Get data from Yahoo Finance using inputed ticker symbol and put data into data frame -- 'df'
+    df = yf.download(ticker, start=start, end=end)
+    
+    return df
