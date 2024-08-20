67e5e4ca18df2d4224ceaebe8d6ffb2c3ca3449d
Daniel Kramer <danielkramer@MacBook-Air-3M.hsd1.il.comcast.net>
2024-08-20 14:08:41 -0500

    random forrest classifier 1st iter

--- data.py
+++ data.py
@@ -1,13 +1,32 @@
 import pandas as pd
 import datetime as dt
+import numpy as np
 import yfinance as yf
+import pandas_ta as ta
 
 def getStockData(ticker):
     # Set time frame for training data
     end = dt.datetime.now()
-    start = end - dt.timedelta(days=365)
+    start = end - dt.timedelta(days=3650) # Days can be adjusted
 
     # Get data from Yahoo Finance using inputed ticker symbol and put data into data frame -- 'df'
-    df = yf.download(ticker, start=start, end=end)
+    df = yf.Ticker(ticker)
+    df = df.history(period="max")
     
+    return df
+
+def convertData(df):
+    df['RSI'] = ta.rsi(df.Close, length=15)
+    df['EMFA'] = ta.ema(df.Close, length=20)
+    df['EMFM'] = ta.ema(df.Close, length=100)
+    df['EMFS'] = ta.ema(df.Close, length=150)
+
+    df['Tomorrow'] = df['Close'].shift(-1)
+
+    df['Target'] = (df['Tomorrow'] > df['Close']).astype(int)
+    
+
+    df.dropna(inplace = True)
+    df.reset_index(inplace = True)
+
     return df
---
+++ model.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from data import getStockData, convertData
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
+
+df = convertData(getStockData("TSLA"))
+print(df)
+
+plt.plot(df['Date'], df['Close'])
+#plt.show()
+
+scaler = MinMaxScaler(feature_range=(-1,1))
+df_scaled = scaler.fit_transform(df.to_numpy()[:,1:])
+
+classifiers = ["Close", "Open", "High", "Low", "Volume", "RSI", "EMFA", "EMFM", "EMFS"]
+
+split = int(len(df) * 0.95)
+
+X_train = df.iloc[:split]
+X_test = df.iloc[split:]
+
+model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
+model.fit(X_train[classifiers], X_train["Target"])
+
+preds = model.predict(X_test[classifiers])
+preds = pd.Series(preds, index=X_test.index)
+
+precision_score(X_test["Target"], preds)
+
+res_table = pd.concat([X_test["Target"], preds], axis=1)
+
+res_table.plot()
+
+def predict(train, test, classifiers, model):
+    model.fit(train[classifiers], train["Target"])
+    preds = model.predict(test[classifiers])
+    preds = pd.Series(preds, index=test.index, name="Predictions")
+    res_table = pd.concat([test["Target"], preds], axis=1)
+    return res_table
+
+def backtest(data, model, classifiers, start=2500, step=250):
+    all_predictions = []
+
+    for i in range(start, data.shape[0], step):
+        train = data.iloc[0:i].copy()
+        test = data.iloc[i:(i+step)].copy()
+        predictions = predict(train, test, classifiers, model)
+        all_predictions.append(predictions)
+    
+    return pd.concat(all_predictions)
+
+predictions = backtest(df, model, classifiers)
+
+print(predictions["Predictions"].value_counts())
+
+print(precision_score(predictions["Target"], predictions["Predictions"]))
